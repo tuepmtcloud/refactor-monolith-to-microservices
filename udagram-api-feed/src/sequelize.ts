ee722da4f1041a412486bb32c3fa9646470b4642
tue.pham <minhtue37@gmail.com>
2023-12-09 22:20:15 +0700

    init

---
+++ udagram-api-feed/src/sequelize.ts
@@ -0,0 +1,18 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  'dialectOptions': {
+    'ssl': {
+      'require': true, // This will help you. But you will see nwe error
+      'rejectUnauthorized': false // This line will fix new error
+    }
+  },
+  'dialect': config.dialect,
+  'storage': ':memory:',
+});
